3bbbd87b7809ef0e710e14447f2415316d3ac960
Matheus <matheus@hotmail.ca>
2024-05-08 15:52:23 -0400

    créer le header

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,17 @@
+<header class="h-24 sm:h-32 flex items-center z-30 w-full">
+    <div class="container mx-auto px-6 flex items-center justify-between">
+        <div class="uppercase text-gray-800 dark:text-white font-black text-3xl">
+        </div>
+        <div class="flex items-center">
+            @include('partials.nav')
+            <button class="lg:hidden flex flex-col ml-4">
+                <span class="w-6 h-1 bg-gray-800 dark:bg-white mb-1">
+                </span>
+                <span class="w-6 h-1 bg-gray-800 dark:bg-white mb-1">
+                </span>
+                <span class="w-6 h-1 bg-gray-800 dark:bg-white mb-1">
+                </span>
+            </button>
+        </div>
+    </div>
+</header>
---
+++ resources/views/partials/nav.blade.php
@@ -0,0 +1,11 @@
+<nav class="font-sen text-gray-800 dark:text-white uppercase text-lg lg:flex items-center hidden">
+    <a href="{{ route('index') }}" class="py-2 px-6 flex">
+        Accueil
+    </a>
+    <a href="{{ route('produits.index') }}" class="py-2 px-6 flex">
+        Catalogue
+    </a>
+    <a href="#" class="py-2 px-6 flex">
+        Contact
+    </a>
+</nav>
