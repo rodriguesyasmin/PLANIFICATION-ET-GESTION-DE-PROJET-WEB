c8f6da7e430810833892a6c07cd27f8f1555449f
Yasmin Rodrigues <yasminbresilienne@hotmail.com>
2024-05-08 15:47:46 -0400

    changer type input pour les tailles du produit, ajouter old pour garder la valeur ajouté par l'utilisateur

--- resources/views/produit/create.blade.php
+++ resources/views/produit/create.blade.php
@@ -5,13 +5,15 @@
 @section('content')
 
     <div class="bg-[#8AD9D7] min-h-screen flex items-center text-lg">
-        <form action="{{ route('produit.store') }}" method="POST" enctype="multipart/form-data" class="p-10 md:w-2/3 lg:w-1/2 mx-auto rounded">
+        <form action="{{ route('produit.store') }}" method="POST" enctype="multipart/form-data"
+            class="p-10 md:w-2/3 lg:w-1/2 mx-auto rounded">
             @csrf
             <div class="shadow">
                 <div class="flex items-center bg-[#F2C749] rounded-b-lg border-[#FF7755] mb-10">
                     <label for="titre" class="w-20 text-right p-4 mr-8 text-[#231F52]">Titre</label>
                     <input type="text" name="titre" id="titre" placeholder="Titre"
-                        class="flex-1 p-4 pl-0 bg-transparent outline-none text-[#231F52] overflow-ellipsis overflow-hidden">
+                        class="flex-1 p-4 pl-0 bg-transparent outline-none text-[#231F52] overflow-ellipsis overflow-hidden"
+                        value="{{ old('titre') }}">
                     @error('titre')
                         <p class="text-red-500 text-lg">{{ $message }}</p>
                     @enderror
@@ -19,6 +21,7 @@
                 <div class="flex items-center bg-[#F2C749] rounded-b-lg border-[#FF7755] mb-10">
                     <label for="description" class="w-20 text-right p-4 mr-8 text-[#231F52]">Description</label>
                     <input type="text" name="description" id="description" placeholder="Description"
+                        value="{{ old('description') }}"
                         class="flex-1 p-4 pl-0 bg-transparent outline-none text-[#231F52] overflow-ellipsis overflow-hidden">
                     @error('description')
                         <p class="text-red-500 text-lg">{{ $message }}</p>
@@ -26,8 +29,14 @@
                 </div>
                 <div class="flex items-center bg-[#F2C749] rounded-b-lg border-[#FF7755] mb-10">
                     <label for="taille" class="w-20 text-right p-4 mr-8 text-[#231F52]">Taille</label>
-                    <input type="text" name="taille" id="taille" placeholder="Taille"
+                    <select name="taille" id="taille"
                         class="flex-1 p-4 pl-0 bg-transparent outline-none text-[#231F52] overflow-ellipsis overflow-hidden">
+                        <option value="S" {{ old('taille') === 'S' ? 'selected' : '' }}>S</option>
+                        <option value="M" {{ old('taille') === 'M' ? 'selected' : '' }}>M</option>
+                        <option value="L" {{ old('taille') === 'L' ? 'selected' : '' }}>L</option>
+                        <option value="XL" {{ old('taille') === 'XL' ? 'selected' : '' }}>XL</option>
+                        <option value="XXL" {{ old('taille') === 'XXL' ? 'selected' : '' }}>XXL</option>
+                    </select>
                     @error('taille')
                         <p class="text-red-500 text-lg">{{ $message }}</p>
                     @enderror
@@ -35,7 +44,8 @@
                 <div class="flex items-center bg-[#F2C749] rounded-b-lg border-[#FF7755] mb-10">
                     <label for="photo" class="w-20 text-right p-4 mr-8 text-[#231F52]">Photo</label>
                     <input type="file" name="photo" id="photo"
-                        class="flex-1 p-4 pl-0 bg-transparent outline-none text-[#231F52] overflow-ellipsis overflow-hidden">
+                        class="flex-1 p-4 pl-0 bg-transparent outline-none text-[#231F52] overflow-ellipsis overflow-hidden"
+                        value="{{ old('photo') }}">
                     @error('photo')
                         <p class="text-red-500 text-lg">{{ $message }}</p>
                     @enderror
@@ -43,7 +53,8 @@
                 <div class="flex items-center bg-[#F2C749] rounded-b-lg border-[#FF7755] mb-10">
                     <label for="photo2" class="w-20 text-right p-4 mr-8 text-[#231F52]">Photo 2</label>
                     <input type="file" name="photo2" id="photo2"
-                        class="flex-1 p-4 pl-0 bg-transparent outline-none text-[#231F52] overflow-ellipsis overflow-hidden">
+                        class="flex-1 p-4 pl-0 bg-transparent outline-none text-[#231F52] overflow-ellipsis overflow-hidden"
+                        value="{{ old('photo2') }}">
                     @error('photo2')
                         <p class="text-red-500 text-lg">{{ $message }}</p>
                     @enderror
@@ -51,7 +62,8 @@
                 <div class="flex items-center bg-[#F2C749] rounded-b-lg border-[#FF7755] mb-10">
                     <label for="photo3" class="w-20 text-right p-4 mr-8 text-[#231F52]">Photo 3</label>
                     <input type="file" name="photo3" id="photo3"
-                        class="flex-1 p-4 pl-0 bg-transparent outline-none text-[#231F52] overflow-ellipsis overflow-hidden">
+                        class="flex-1 p-4 pl-0 bg-transparent outline-none text-[#231F52] overflow-ellipsis overflow-hidden"
+                        value="{{ old('photo3') }}">
                     @error('photo3')
                         <p class="text-red-500 text-lg">{{ $message }}</p>
                     @enderror
@@ -59,7 +71,8 @@
                 <div class="flex items-center bg-[#F2C749] rounded-b-lg border-[#FF7755] mb-10">
                     <label for="categorie" class="w-20 text-right p-4 mr-8 text-[#231F52]">Catégorie</label>
                     <input type="text" name="categorie" id="categorie" placeholder="Catégorie"
-                        class="flex-1 p-4 pl-0 bg-transparent outline-none text-[#231F52] overflow-ellipsis overflow-hidden">
+                        class="flex-1 p-4 pl-0 bg-transparent outline-none text-[#231F52] overflow-ellipsis overflow-hidden"
+                        value="{{ old('categorie') }}">
                     @error('categorie')
                         <p class="text-red-500 text-lg">{{ $message }}</p>
                     @enderror
@@ -67,7 +80,8 @@
                 <div class="flex items-center bg-[#F2C749] rounded-b-lg border-[#FF7755] mb-10">
                     <label for="quantite" class="w-20 text-right p-4 mr-8 text-[#231F52]">Quantité</label>
                     <input type="number" name="quantite" id="quantite" placeholder="Quantité"
-                        class="flex-1 p-4 pl-0 bg-transparent outline-none text-[#231F52] overflow-ellipsis overflow-hidden">
+                        class="flex-1 p-4 pl-0 bg-transparent outline-none text-[#231F52] overflow-ellipsis overflow-hidden"
+                        value="{{ old('quantite') }}">
                     @error('quantite')
                         <p class="text-red-500 text-lg">{{ $message }}</p>
                     @enderror
