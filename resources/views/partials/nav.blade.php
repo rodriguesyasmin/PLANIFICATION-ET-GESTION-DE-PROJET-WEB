b7b1874b7705d8f78859677390d2839680ba7e44
Matheus <matheus@hotmail.ca>
2024-05-08 16:56:21 -0400

    ajuster header et nav

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,8 +1,8 @@
 <nav class="font-sen text-gray-800 dark:text-white uppercase text-lg lg:flex items-center hidden">
-    <a href="{{ route('index') }}" class="py-2 px-6 flex">
+    <a href="" class="py-2 px-6 flex">
         Accueil
     </a>
-    <a href="{{ route('produits.index') }}" class="py-2 px-6 flex">
+    <a href="{{ route('produit.index') }}" class="py-2 px-6 flex">
         Catalogue
     </a>
     <a href="#" class="py-2 px-6 flex">
