acf8f8b01d509d85aca3ce0e617edecbd31ee54a
Matheus <matheus@hotmail.ca>
2024-05-10 16:15:22 -0400

    ajouter page produit-show

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,5 +1,5 @@
 <nav class="font-sen text-gray-800 dark:text-white uppercase text-lg lg:flex items-center hidden">
-    <a href="" class="py-2 px-6 flex">
+    <a href="{{ route('index') }}" class="py-2 px-6 flex">
         Accueil
     </a>
     <a href="{{ route('produit.index') }}" class="py-2 px-6 flex">
