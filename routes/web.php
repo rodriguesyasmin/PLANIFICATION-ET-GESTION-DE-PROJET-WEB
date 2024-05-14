871a5fbdad079a1a3db66f2ef7bf19a5843ca00c
Yasmin Rodrigues <yasminbresilienne@hotmail.com>
2024-05-14 12:30:19 -0400

    ajuster route pour l'index

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProduitController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Http\Request;
 use App\Http\Controllers\StripeController;
 
-Route::get('/', function (Request $request) {
-    return view('index');
-})->name('index');
+Route::get('/', [HomeController::class, 'index'])->name('index');
 
 Route::resource('/produit', ProduitController::class); {
 }
@@ -15,7 +14,3 @@ Route::resource('/produit', ProduitController::class); {
 Route::get('/stripe', 'App\Http\Controllers\StripeController@checkout')->name('checkout');
 Route::post('/stripe/test', 'App\Http\Controllers\StripeController@test');
 Route::get('/stripe/success', 'App\Http\Controllers\StripeController@success')->name('success');
-
-
-// Route::get('stripe', [StripeController::class, 'stripe']);
-// Route::post('stripe', [StripeController::class, 'stripePost'])->name('stripe.post');
