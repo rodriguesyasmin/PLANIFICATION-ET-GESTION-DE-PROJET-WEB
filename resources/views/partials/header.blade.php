47ac3bb068d3fd6b1daa5a4e5a1a627fc34e5a83
Matheus <matheus@hotmail.ca>
2024-05-14 13:18:25 -0400

    modification dans le header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,11 +1,12 @@
 <header class="h-24 sm:h-32 flex items-center z-30 w-full">
     <div class="container mx-auto px-6 flex items-center justify-between">
-        <div class="uppercase text-gray-800 dark:text-white font-black text-3xl">
+        <div class="uppercase text-gray-800 font-black text-3xl">
             Super Clothes
         </div>
-        
-        <div >
-            <img src="{{ Vite::asset('resources/img/SuperClothes_icon_transparent.png') }}" class="h-40 mr-6 sm:h-15" alt="Super Clothes Logo">
+
+        <div>
+            <img src="{{ Vite::asset('resources/img/SuperClothes_icon_transparent.png') }}" class="h-40 mr-6 sm:h-15"
+                alt="Super Clothes Logo">
         </div>
         <div class="flex items-center">
             @include('partials.nav')
