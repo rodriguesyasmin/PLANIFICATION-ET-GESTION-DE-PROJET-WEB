1ab05d495606044786ecafa92f8ae3fe35abf68f
Tiago Barros <tiagolp@msn.com>
2024-05-15 12:41:46 -0400

    ajouter style view show et index

--- resources/views/produit/index.blade.php
+++ resources/views/produit/index.blade.php
@@ -4,16 +4,19 @@
 
 @section('content')
 
-    <main>
-        <div class="p-5 sm:p-8 bg-[#231F52]">
-            <div class="columns-1 gap-5 sm:columns-2 sm:gap-8 md:columns-3 lg:columns-4 [&>img:not(:first-child)]:mt-8">
-                @foreach ($produits as $produit)
-                    <a class="max-w-xs" href="{{ route('produit.show', ['produit' => $produit->id]) }}">
-                        <img src="{{ Vite::asset('storage/app/public/' . $produit->photo) }}" alt="{{ $produit->titre }}">
-                    </a>
-                @endforeach
+    <div class="flex flex-col min-h-screen">
+        <main class="flex-grow">
+            <div class="sm:p-8 bg-[#231F52] flex-grow">
+                <div
+                    class="w-fit mx-auto grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 justify-items-center justify-center gap-y-20 gap-x-20 mt-10 mb-5">
+                    @foreach ($produits as $produit)
+                        <a class="max-w-xs" href="{{ route('produit.show', ['produit' => $produit->id]) }}">
+                            <img class="rounded-xl" src="{{ Vite::asset('storage/app/public/' . $produit->photo) }}"
+                                alt="{{ $produit->titre }}">
+                        </a>
+                    @endforeach
+                </div>
             </div>
-        </div>
-    </main>
+        </main>
 
-@endsection
+    @endsection
--- resources/views/produit/show.blade.php
+++ resources/views/produit/show.blade.php
@@ -5,11 +5,11 @@
 @section('content')
 
     <main>
-        <div class=" dark:bg-gray-800 py-8">
+        <div class=" py-8">
             <div class="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div class="flex flex-col md:flex-row  bg-[#8AD9D7] -mx-4 p-10 rounded-2xl">
                     <div class="md:flex-1  px-4">
-                        <div class="h-[460px] rounded-lg  dark:bg-gray-700 mb-4">
+                        <div class="h-[460px] rounded-lg  mb-4">
                             <img class="w-full h-full object-cover rounded-2xl"
                                 src="{{ Vite::asset('storage/app/public/' . $produit->photo) }}"
                                 alt="storage/{{ $produit->titre }}">
@@ -19,27 +19,27 @@
                                 <form action="stripe/test" method="POST">
                                     <input type="hidden" name="_token" value="{{ csrf_token() }}">
                                     <button type="submit" id="checkout-test-button"
-                                        class="w-full bg-gray-900 dark:bg-gray-600 text-white py-2 px-4 rounded-full font-bold hover:bg-gray-800 dark:hover:bg-gray-700">Checkout
+                                        class="w-full bg-gray-900 text-white py-2 px-4 rounded-full font-bold hover:bg-gray-800 dark:hover:bg-gray-700">Checkout
                                         (Test)</button>
                                 </form>
                             </div>
                         </div>
                     </div>
                     <div class="md:flex-1 px-4">
-                        <h2 class="text-2xl font-bold text-gray-800 dark:text-white mb-2">{{ $produit->titre }}</h2>
+                        <h2 class="text-2xl font-bold dark:text-white mb-2">{{ $produit->titre }}</h2>
                         <div class="flex mb-4">
                             <div class="mr-4">
-                                <span class="font-bold text-gray-700 dark:text-gray-300">Price:</span>
-                                <span class="text-gray-600 dark:text-gray-300">{{ $produit->prix }}</span>
+                                <span class="font-bold dark:text-gray-300">Price:</span>
+                                <span class="dark:text-gray-300">{{ $produit->prix }}</span>
                             </div>
                             <div>
-                                <span class="font-bold text-gray-700 dark:text-gray-300">Availability:</span>
+                                <span class="font-bold dark:text-gray-300">Availability:</span>
                                 <span
                                     class="text-gray-600 dark:text-gray-300">{{ $produit->quantite < 1 ? 'Out of Stock' : 'In Stock' }}</span>
                             </div>
                         </div>
                         <div class="mb-4">
-                            <span class="font-bold text-gray-700 dark:text-gray-300">Select Size:</span>
+                            <span class="font-bold dark:text-gray-300">Select Size:</span>
                             <div class="flex items-center mt-2">
                                 <button
                                     class="bg-gray-300 dark:bg-gray-700 text-gray-700 dark:text-white py-2 px-4 rounded-full font-bold mr-2 hover:bg-gray-400 dark:hover:bg-gray-600">S</button>
