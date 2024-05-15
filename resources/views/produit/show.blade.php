b730d0ab0f217a1bed1e853afbab26831d68a961
Matheus <matheus@hotmail.ca>
2024-05-15 10:39:38 -0400

    reajuster style de la page show

--- resources/views/produit/show.blade.php
+++ resources/views/produit/show.blade.php
@@ -19,43 +19,43 @@
                                 <form action="stripe/test" method="POST">
                                     <input type="hidden" name="_token" value="{{ csrf_token() }}">
                                     <button type="submit" id="checkout-test-button"
-                                        class="w-full bg-gray-900 dark:bg-gray-600 text-white py-2 px-4 rounded-full font-bold hover:bg-gray-800 dark:hover:bg-gray-700">Checkout
+                                        class="w-full bg-gray-900 dark:bg-gray-600  py-2 px-4 rounded-full text-white hover:bg-gray-800 dark:hover:bg-gray-700">Checkout
                                         (Test)</button>
                                 </form>
                             </div>
                         </div>
                     </div>
                     <div class="md:flex-1 px-4">
-                        <h2 class="text-2xl font-bold text-gray-800 dark:text-white mb-2">{{ $produit->titre }}</h2>
+                        <h2 class="text-2xl font-bold text-gray-800 mb-2">{{ $produit->titre }}</h2>
                         <div class="flex mb-4">
                             <div class="mr-4">
-                                <span class="font-bold text-gray-700 dark:text-gray-300">Price:</span>
-                                <span class="text-gray-600 dark:text-gray-300">{{ $produit->prix }}</span>
+                                <span class="font-bold text-gray-700 ">Price:</span>
+                                <span class="text-gray-600">{{ $produit->prix }}</span>
                             </div>
                             <div>
-                                <span class="font-bold text-gray-700 dark:text-gray-300">Availability:</span>
+                                <span class="font-bold text-gray-700 ">Availability:</span>
                                 <span
-                                    class="text-gray-600 dark:text-gray-300">{{ $produit->quantite < 1 ? 'Out of Stock' : 'In Stock' }}</span>
+                                    class="text-gray-600 ">{{ $produit->quantite < 1 ? 'Out of Stock' : 'In Stock' }}</span>
                             </div>
                         </div>
                         <div class="mb-4">
-                            <span class="font-bold text-gray-700 dark:text-gray-300">Select Size:</span>
+                            <span class="font-bold text-gray-700 ">Select Size:</span>
                             <div class="flex items-center mt-2">
                                 <button
-                                    class="bg-gray-300 dark:bg-gray-700 text-gray-700 dark:text-white py-2 px-4 rounded-full font-bold mr-2 hover:bg-gray-400 dark:hover:bg-gray-600">S</button>
+                                    class="bg-gray-300 dark:bg-gray-700 text-gray-700 dark: py-2 px-4 rounded-full font-bold mr-2 hover:bg-gray-400 dark:hover:bg-gray-600">S</button>
                                 <button
-                                    class="bg-gray-300 dark:bg-gray-700 text-gray-700 dark:text-white py-2 px-4 rounded-full font-bold mr-2 hover:bg-gray-400 dark:hover:bg-gray-600">M</button>
+                                    class="bg-gray-300 dark:bg-gray-700 text-gray-700 dark: py-2 px-4 rounded-full font-bold mr-2 hover:bg-gray-400 dark:hover:bg-gray-600">M</button>
                                 <button
-                                    class="bg-gray-300 dark:bg-gray-700 text-gray-700 dark:text-white py-2 px-4 rounded-full font-bold mr-2 hover:bg-gray-400 dark:hover:bg-gray-600">L</button>
+                                    class="bg-gray-300 dark:bg-gray-700 text-gray-700 dark: py-2 px-4 rounded-full font-bold mr-2 hover:bg-gray-400 dark:hover:bg-gray-600">L</button>
                                 <button
-                                    class="bg-gray-300 dark:bg-gray-700 text-gray-700 dark:text-white py-2 px-4 rounded-full font-bold mr-2 hover:bg-gray-400 dark:hover:bg-gray-600">XL</button>
+                                    class="bg-gray-300 dark:bg-gray-700 text-gray-700 dark: py-2 px-4 rounded-full font-bold mr-2 hover:bg-gray-400 dark:hover:bg-gray-600">XL</button>
                                 <button
-                                    class="bg-gray-300 dark:bg-gray-700 text-gray-700 dark:text-white py-2 px-4 rounded-full font-bold mr-2 hover:bg-gray-400 dark:hover:bg-gray-600">XXL</button>
+                                    class="bg-gray-300 dark:bg-gray-700 text-gray-700 dark: py-2 px-4 rounded-full font-bold mr-2 hover:bg-gray-400 dark:hover:bg-gray-600">XXL</button>
                             </div>
                         </div>
                         <div>
-                            <span class="font-bold text-gray-700 dark:text-gray-300">Product Description:</span>
-                            <p class="text-gray-600 dark:text-gray-300 text-sm mt-2">
+                            <span class="font-bold text-gray-700">Product Description:</span>
+                            <p class="text-gray-600  text-sm mt-2">
                                 {{ $produit->description }}
                             </p>
                         </div>
